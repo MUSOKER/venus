64e76cfbc7caa89bcfeac5773ea15ad8c5d468f9
ogwalu chidozie <ogwaluchidozie@gmail.com>
2023-05-22 22:12:52 +0100

    posted internship model updated

---
+++ src/models/posted_internship.model.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const postedInternshipSchema = new Schema(
+  {
+    categoryId: {
+      type: Array,
+      required: true,
+    },
+    internshipTitle: {
+      type: String,
+      required: true,
+    },
+    internshipDescription: {
+      type: String,
+      required: true,
+    },
+    internshipType: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: String,
+    },
+    Skills: {
+      type: [String],
+    },
+    responsibilities: {
+      type: [String],
+    },
+    qualification: {
+      type: String,
+    },
+    internshipEndDate: {
+      type: Date,
+    },
+    numberOfOpenings: {
+      type: Number,
+      default: 1,
+    },
+    internshipStatus: {
+      type: String,
+      enum: ['Active', 'Inactive'],
+      default: 'Active',
+    },
+    internshipLocation:{
+        type :String,
+        default: 'Point',
+    },
+    experienceLevel:{
+        type: String,
+        enum: ['Entry_Level', 'Junior_Level', 'Mid_Level', 'Senior_Level'],
+    },
+    companyId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company',
+      required: true,
+    },
+    ApprovedAt: {
+      type: Date,
+    },
+    approvedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    status: {
+      type: String,
+      enum: ['Approved', 'Rejected', 'Pending'],
+      default: 'Pending',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const PostedInternship = mongoose.model('PostedInternship', postedInternshipSchema);
+
+module.exports = PostedInternship;
