c97163d4c0157675ad3724f615a3df9b57d67678
ogwalu chidozie <ogwaluchidozie@gmail.com>
2023-05-22 22:14:10 +0100

    Added applied and posted internship module export

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,14 @@ const UserModel = require('./user.model');
 const AddressModel = require('./address.model');
 const AdminModel = require('./admin.model');
 const UserIndentificationSchema = require('./user_identification.model');
+const AppliedInternshipSchema = require('./applied_internship.model');
+const PostedInternshipSchema = require('./posted_internship.model');
 
 module.exports = {
   UserModel,
   AddressModel,
   AdminModel,
   UserIndentificationSchema,
+  AppliedInternshipSchema,
+  PostedInternshipSchema
 };
