87cb17dd9d18a40f0ff19a38477bf4c0f8bcb0ae
ochosteve <ochosteve08@gmail.com>
2023-05-24 19:50:17 +0100

    fixed error on function mismatch in router-category index file

--- src/routes/category.route.js/index.js
+++ src/routes/category.route.js/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { error } = require('@Enseedling/enseedling-lib-handler');
-// const { getCategoryRoutes } = require('./get.category.route');
 const { getCategoryRoutes } = require('./get.category.route');
 const { deleteCategoryRoutes } = require('./delete.category.route');
 const { createCategoryRoutes } = require('./create.category.route');
@@ -12,6 +11,4 @@ categoryRoutes.use(deleteCategoryRoutes);
 categoryRoutes.use(createCategoryRoutes);
 categoryRoutes.use('*', () => error.throwNotFound({ item: 'Route' }));
 
-module.exports = {
-  categoryRoutes,
-};
+module.exports = categoryRoutes;
