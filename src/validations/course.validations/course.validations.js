1ee5f12feee8d1b8864197551ddee8be0dc2ec66
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-05-25 13:42:23 +0100

    create course endpoint

--- src/validations/course.validations/course.validations.js
+++ src/validations/course.validations/course.validations.js
@@ -4,20 +4,30 @@ const { PROJECT_STATUS } = require('../../const');
 const { PROJECT_VISIBILITY } = require('../../const');
 
 const addCoursetValidation = Joi.object().keys({
+    course_name: enseedlingValidator.generic.string.any.required().label('Course Name'),
     comments: Joi.array()
         .items(enseedlingValidator.generic.string.any)
         .label('Comments'),
-    visibility: enseedlingValidator.generic.number.integer.valid(PROJECT_VISIBILITY.public, PROJECT_VISIBILITY.private).label('Visibility'),
+    course_description: enseedlingValidator.generic.string.any.required().label('Course Description'),
+    course_duration: enseedlingValidator.generic.number.required().label('Course Duration'),
+    course_rating: enseedlingValidator._id.required().label('Course Rating'),
+    course_thumb_image: enseedlingValidator.generic.string.medium.required().label('Course Thumb Image'),
+    course_state: enseedlingValidator.generic.string.medium.label('Course State'),
+    category_ids: enseedlingValidator._id.required().label('Category'),
+    social_media_links: Joi.array()
+        .items(enseedlingValidator.generic.string.any)
+        .label('Social Medial Links'),
+    total_videos: enseedlingValidator.generic.number.label('Total Video'),
     startDate: Joi.date().required().label('Start Date'),
     endDate: Joi.date().required().label('End Date'),
-    course_name: enseedlingValidator.generic.string.any.required().label('Course Name'),
-    course_description: enseedlingValidator.generic.string.any.required().label('Course Description'),
-    projectLink: enseedlingValidator.generic.string.medium.required().label('Project Link'),
-    projectImage: enseedlingValidator.generic.string.medium.label('Project Image'),
-    projectMilestone: enseedlingValidator.generic.string.medium.label('Project Milestone'),
-    course_duration: enseedlingValidator.generic.number.required().label('Course Duration'),
-    category_ids: enseedlingValidator._id.required().label('Category'),
-    userId: enseedlingValidator._id.required().label('User Id'),
+    capacity: enseedlingValidator.generic.number.required().label('Capacity'),
+    status: enseedlingValidator.generic.string.medium.label('Course Status'),
+    createdBy: enseedlingValidator._id.required().label('Created By'),
+    instructorId: enseedlingValidator._id.required().label('Insructor'),
+    demo_video_src: enseedlingValidator.generic.string.medium.required().label('Demo Video src'),
+    meta_info: Joi.array()
+        .items(enseedlingValidator.generic.string.any)
+        .label('Meta Info'),
 });
 
 const updateCourseValidation = Joi.object().keys({
