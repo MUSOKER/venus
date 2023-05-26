22e49a888e820c6a2b5a0524f289b03ef45cf8a8
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-05-26 08:57:52 +0100

    adding back

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,6 @@
 const userServices = require('./user.services');
 const projectServices = require('./project.services');
 const notificationDispatcher = require('./thirdParty');
-const projectServices = require('./project.services');
 const courseServices = require('./course.services');
 
 module.exports = {
