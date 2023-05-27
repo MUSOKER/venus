68506ce75754d0da227cb818cd1a65b3176806ea
ogwalu chidozie <ogwaluchidozie@gmail.com>
2023-05-27 10:49:17 +0100

    updated validation

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,9 +1,11 @@
 const userValidation = require('./user.validations');
 const projectValidation = require('./project.validations');
 const categoryValidation = require('./category.validations');
+const internshipValidation = require('./internship.validations');
 
 module.exports = {
   userValidation,
   projectValidation,
   categoryValidation,
+  internshipValidation,
 };
--- src/validations/internship.validations/applied.internship.validations.js
+++ src/validations/internship.validations/applied.internship.validations.js
@@ -2,13 +2,13 @@ const Joi = require('joi');
 const enseedlingValidator = require('@Enseedling/enseedling-validations');
 
 const appliedInternshipValidation = Joi.object({
-  user_id: enseedlingValidator.generic.string.medium.required().label('User ID'),
-  internship_id: enseedlingValidator.generic.string.medium.required().label('Internship Id'),
-  selection_status: Joi.string()
+  userId: enseedlingValidator.generic.string.medium.required().label('User ID'),
+  internshipId: enseedlingValidator.generic.string.medium.required().label('Internship Id'),
+  selectionStatus: Joi.string()
     .valid('Hired', 'Rejected', 'Shortlisted', 'pending')
     .default('Pending')
     .label('Selection Status'),
-  additional_information: Joi.string().allow('').optional().label('Additional Information'),
+  additionalInformation: Joi.string().allow('').optional().label('Additional Information'),
 });
 
 module.exports = {
