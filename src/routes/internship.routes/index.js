b40eceb63537b9e41e6236beb941f00a07d7732f
ogwalu chidozie <ogwaluchidozie@gmail.com>
2023-05-27 10:46:18 +0100

    updated route

--- src/routes/internship.routes/index.js
+++ src/routes/internship.routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+
 const { error } = require('@Enseedling/enseedling-lib-handler');
+const applyInternshipRoutes = require('./apply.internship.routes');
 
 const internshipRoutes = express.Router();
-internshipRoutes.use(internshipRoutes);
+
+internshipRoutes.use(applyInternshipRoutes);
 
 internshipRoutes.use('*', () => error.throwNotFound({ item: 'Route' }));
 
